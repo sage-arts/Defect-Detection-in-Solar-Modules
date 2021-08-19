7a6689a3f8cd5c0277eeac0f2bfdcccb2f9d8b7e
Aman <amanjaipuria086@gmail.com>
2021-08-19 21:01:03 +0530

    load test-train data

---
+++ code/load_train_set.py
@@ -0,0 +1,31 @@
+
+from PIL import Image
+import numpy as np
+import os
+
+
+def load_train_set(fname=None):
+    if fname is None:
+        # Assume we are in the utils folder and get the absolute path to the
+        # parent directory.
+        path=os.getcwd()
+        fname = os.path.abspath(os.path.join(path,
+                                             os.path.pardir))
+        fname = os.path.join(fname, 'labels.csv')
+
+    data = np.genfromtxt(fname, dtype=['|S19', '<f8', '|S4'], names=[
+                         'path', 'probability', 'type'])
+    image_fnames = np.char.decode(data['path'])
+    probs = data['probability']
+    types = np.char.decode(data['type'])
+
+    def load_cell_image(fname):
+        with Image.open(fname) as image:
+            return np.asarray(image)
+
+    dir = os.path.dirname(fname)
+
+    images = np.array([load_cell_image(os.path.join(dir, fn))
+                       for fn in image_fnames])
+
+    return images, probs, types
